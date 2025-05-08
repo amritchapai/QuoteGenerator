d1b7415c39ec0af8a2cab9f8a6b644f24e4b0838
amritchapai <chapaiamrit@gmail.com>
2025-05-08 21:22:14 +0545

    fixed for size and quote container

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
+//for changing the mode and sliding
 const toggle = document.getElementById("switch");
 toggle.addEventListener("click", () => {
   document.getElementById("slider").classList.toggle("slider-slide");
   document.getElementById("body").classList.toggle("dark");
 });
 
+//for changing the font-size
 const range = document.getElementById("range");
 range.addEventListener("input", () => {
   console.log(range.value);
   const size = range.value;
   document.getElementById("font-size").textContent = size;
-  document.getElementById("quote-container").style.fontSize = size + "px";
+  document.getElementById("quote").style.fontSize = size + "px";
 });
