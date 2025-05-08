9d4381db6f3c7ed1e067408ddc63b4e04ffe8a1a
amritchapai <chapaiamrit@gmail.com>
2025-05-08 20:27:10 +0545

    font size logic done

--- main.js
+++ main.js
@@ -3,3 +3,11 @@ toggle.addEventListener("click", () => {
   document.getElementById("slider").classList.toggle("slider-slide");
   document.getElementById("body").classList.toggle("dark");
 });
+
+const range = document.getElementById("range");
+range.addEventListener("input", () => {
+  console.log(range.value);
+  const size = range.value;
+  document.getElementById("font-size").textContent = size;
+  document.getElementById("quote-container").style.fontSize = size + "px";
+});
