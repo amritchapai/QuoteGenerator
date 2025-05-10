2c700378b28dbc67d51cba9ca72332d5c464952a
amritchapai <chapaiamrit@gmail.com>
2025-05-10 11:05:48 +0545

    quote block and right left

---
+++ data.js
@@ -0,0 +1,51 @@
+const sampleData = [
+  {
+    id: 1,
+    quote:
+      "Science is a way of thinking much more than it is a body of knowledge.",
+    category: "science",
+  },
+  {
+    id: 2,
+    quote:
+      "The important thing in science is not so much to obtain new facts as to discover new ways of thinking about them.",
+    category: "science",
+  },
+  {
+    id: 3,
+    quote: "You miss 100% of the shots you don't take.",
+    category: "sports",
+  },
+  {
+    id: 4,
+    quote: "Champions keep playing until they get it right.",
+    category: "sports",
+  },
+  {
+    id: 5,
+    quote: "Nepal is not only about mountains, it's about resilience.",
+    category: "nepali",
+  },
+  {
+    id: 6,
+    quote: "नेपाल सुन्दरताको देश मात्र होइन, साहस र आत्मबलको पनि प्रतीक हो।",
+    category: "nepali",
+  },
+  {
+    id: 7,
+    quote: "Somewhere, something incredible is waiting to be known.",
+    category: "science",
+  },
+  {
+    id: 8,
+    quote: "Winners never quit and quitters never win.",
+    category: "sports",
+  },
+  {
+    id: 9,
+    quote: "सगरमाथा हाम्रो गौरव हो, हाम्रो चिनारी हो।",
+    category: "nepali",
+  },
+];
+
+export default sampleData;
--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import sampleData from "./data.js";
+
 //for changing the mode and sliding
 const toggle = document.getElementById("switch");
 toggle.addEventListener("click", () => {
@@ -12,4 +14,19 @@ range.addEventListener("input", () => {
   const size = range.value;
   document.getElementById("font-size").textContent = size;
   document.getElementById("quote").style.fontSize = size + "px";
+});
+
+const category = document.getElementById("category");
+console.log(category.value);
+category.addEventListener("input", () => {
+  console.log(category.value);
+});
+
+const generatorButton = document.getElementById("generator");
+generatorButton.addEventListener("click", () => {
+  const quote = document.getElementById("quote");
+  const randomIndex = Math.floor(Math.random() * sampleData.length);
+  console.log(randomIndex);
+  console.log(sampleData[randomIndex].quote);
+  quote.textContent = sampleData[randomIndex].quote;
 });
